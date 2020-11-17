0aece37ebc829462c7fba88b8ab7e0356cc7f0c6
Viviana Garcia <vivianandrea4@gmail.com>
2020-11-17 17:55:53 -0500

    vivi iteration

--- src/App.js
+++ src/App.js
@@ -3,25 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import contacts from './contacts.json'
 
+let firstFive = contacts.splice(0, 5)
+
 function App() {
- 
- const [showing,setShowing] = useState(contacts.splice(0,5))
- console.log(showing)
- 
- 
-  const DisplayFirstFive = () =>{
+
+  const [showing, setShowing] = useState(firstFive)
+  // const [contactsLeft, setContactsLeft] = useState(contacts)
+  console.log(showing)
+  // console.log(contactsLeft)
+
+
+  const DisplayFirstFive = () => {
     return showing.map((contact) => {
       return (
         <tr>
-          <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+          <td><img src={contact.pictureUrl} alt={contact.name} /></td>
           <td>{contact.name}</td>
-          <td>{Math.round((contact.popularity)*100)/100}</td>  
+          <td>{Math.round((contact.popularity) * 100) / 100}</td>
         </tr>
-          
-        )  
-    })
-}
 
+      )
+    })
+  }
+  const AddRandomContact = () => {
+
+    let randomContact = contacts.splice(Math.floor(Math.random() * contacts.length), 1)
+    setShowing([...showing, ...randomContact])
+
+
+  }
 
 
 
@@ -31,20 +41,21 @@ function App() {
 
   return (
     <div className="App">
-     <h1>Iron Contacts</h1>
-     <table>
-     <thead>
-     <tr>
-       <th>Picture</th>
-       <th>Name</th>
-       <th>Popularity</th>
-     </tr>
-     </thead>
-     <tbody>
-     <DisplayFirstFive/> 
-    
-     </tbody>
-     </table>
+      <button onClick={(AddRandomContact)}>Add Random Contact</button>
+      <h1>Iron Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <DisplayFirstFive />
+
+        </tbody>
+      </table>
 
     </div>
   );
